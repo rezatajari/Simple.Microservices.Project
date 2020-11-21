ca3cbd1b1a19524cd5513b411aa4664b6306b676
rezatajari <reza.tajari70@gmail.com>
2020-11-21 21:44:24 +0330

    Create CatalogController

---
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    public class CatalogController : ControllerBase
+    {
+
+
+
+    }
+}
