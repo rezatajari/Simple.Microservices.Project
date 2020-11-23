3addc748648efdcfee44a798a9a40c6b4b2140dd
rezatajari <reza.tajari70@gmail.com>
2020-11-23 20:14:34 +0330

    Create "IProductRepository" & "ProductRepository"
    Create's:
    - GetProducts
    - GetProduct by id
    - GetProductByName
    - GetProductByCategory
    - Create a product
    - Update a product
    - Delete a product
    all of theirs in ProductRepository

---
+++ src/Catalog/Catalog.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories.Interfaces
+{
+    /// <summary>
+    /// اینترفیس برای انواع دستورالعمل هایی که با دیتابیس مونگو برای کاتالوگ خواهیم داشت
+    /// </summary>
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task Create(Product product);
+        Task<bool> Update(Product product);
+        Task<bool> Delete(string id);
+    }
+}
---
+++ src/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,123 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <summary>
+        /// لیست کل محصولات
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context
+                         .Products
+                         .Find(p => true)
+                         .ToListAsync();
+        }
+
+        /// <summary>
+        /// لیست محصول مورد نظر براساس آیدی دریافتی
+        /// </summary>
+        /// <param name="id">آیدی محصول خواسته شده</param>
+        /// <returns></returns>
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _context
+                         .Products
+                         .Find(p => p.Id == id)
+                         .FirstOrDefaultAsync();
+
+        }
+
+        /// <summary>
+        /// لیست محصول/محصولات براساس اسم
+        /// </summary>
+        /// <param name="name">اسم محصول درخواستی</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+
+            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Name, name);
+
+            return await _context
+                         .Products
+                         .Find(filter)
+                         .ToListAsync();
+        }
+
+        /// <summary>
+        /// لیست محصول/محصولات براساس دسته بندی
+        /// </summary>
+        /// <param name="categoryName">اسم دسته بندی درخواستی</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+
+            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Category, categoryName);
+
+            return await _context
+                         .Products
+                         .Find(filter)
+                         .ToListAsync();
+
+        }
+
+        /// <summary>
+        /// ایجاد کردن محصول جدید
+        /// </summary>
+        /// <param name="product">مشخصات محصول جدید</param>
+        /// <returns></returns>
+        public async Task Create(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);
+        }
+
+        /// <summary>
+        /// بروزرسانی محصول مورد نظر
+        /// </summary>
+        /// <param name="product">محصول نیاز به بروزرسانی</param>
+        /// <returns></returns>
+        public async Task<bool> Update(Product product)
+        {
+            var updateResult = await _context
+                                     .Products
+                                     .ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updateResult.IsAcknowledged
+                && updateResult.ModifiedCount > 0;
+        }
+
+        /// <summary>
+        /// حذف کردن محصول مورد نظر
+        /// </summary>
+        /// <param name="product">محصول نیاز به حذف</param>
+        /// <returns></returns>
+        public async Task<bool> Delete(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _context
+                                            .Products
+                                            .DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged
+                && deleteResult.DeletedCount > 0;
+
+        }
+    }
+}
