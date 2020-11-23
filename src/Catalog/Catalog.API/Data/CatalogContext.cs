320703e9cc6b4aa11580f0b39529d06d0ed997c6
rezatajari <reza.tajari70@gmail.com>
2020-11-23 18:46:58 +0330

    Create "CatalogContextSeed" and process on products collection

--- src/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -10,13 +10,18 @@ namespace Catalog.API.Data
 
         public CatalogContext(ICatalogDatabaseSettings settings)
         {
+            // اتصال برقرار کردن به دیتابیس مونگو
             var client = new MongoClient(settings.ConnectionString);
+
+            // انتخاب دیتابیس مونگو برای کاتالوگ
             var database = client.GetDatabase(settings.DatabaseName);
 
+            // ست کردن کالکشن پروداکت از داخل دیتابیس مونگو
             Products = database.GetCollection<Product>(settings.CollectionName);
+
+            // فرستادن اطلاعات کالکشن برای بررسی داده های آن
+            CatalogContextSeed.SeedData(Products);
         }
-
-
 
         public IMongoCollection<Product> Products { get; }
     }
