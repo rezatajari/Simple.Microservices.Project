00f4433cf078b1df6725ac506a2cb07dcaa27d94
rezatajari <reza.tajari70@gmail.com>
2020-11-26 19:54:15 +0330

    Add Http verbs in BasketController

--- src/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,13 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.API.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
     public class BasketController : ControllerBase
     {
-     
+
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        /// <summary>
+        /// گرفتن مشخصات سبد کاربر براساس نام کاربر
+        /// </summary>
+        /// <param name="userName">نام کاربر</param>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> GetBasket(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket);
+        }
+
+        /// <summary>
+        /// بروزرسانی یا ساخت سبد جدید
+        /// </summary>
+        /// <param name="basket">مشخصات سبد برای بروزرسانی یا ایجاد شدن</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(BasketCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<BasketCart>> UpdateBasket([FromBody] BasketCart basket)
+        {
+            return Ok(await _repository.UpdateBasket(basket));
+        }
+
+        /// <summary>
+        /// حذف کردن یک سبد بوسیله نام کاربر مورد نظر به آن سبد
+        /// </summary>
+        /// <param name="userName">نام کاربر</param>
+        /// <returns></returns>
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            return Ok(await _repository.DeleteBasket(userName));
+        }
     }
 }
