bbb80d1d0d4997325c40a1a500496ec1501613d0
rezatajari <reza.tajari70@gmail.com>
2020-11-29 22:14:49 +0330

    Create "EventBusRabbitMQProducer"
    In this class we done implementation some things:
    - Create channel
    - Create queue in that channel
    - After above section implentation publish message as a publisher

---
+++ src/Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,58 @@
+﻿using EventBusRabbitMQ.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _connection;
+
+        public EventBusRabbitMQProducer(IRabbitMQConnection connection)
+        {
+            _connection = connection ?? throw new ArgumentNullException(nameof(connection));
+        }
+
+        /// <summary>
+        /// ایجاد کردن کانال برای صف بندی رویداد های مدل شده از سوی سبد 
+        /// و فرستادن آن به رببیت ام کیو
+        /// </summary>
+        /// <param name="queueName">نام صف</param>
+        /// <param name="publishModel">مدل درخواستی از سوی سبد</param>
+        public void PublishBasketCheckout(string queueName, BasketCheckoutEvent publishModel)
+        {
+            using (var channel = _connection.CreateModel())
+            {
+                // ساخت صف برای ذخیره پیام که همان مدل سبد ما می باشد
+                // در هنگام ساخت صف مشخصاتی از قبیل ذخیره شدن صف در دیتابیس 
+                // حذف کردن صف به صورت خودکار و یکسری مشخصات دیگه می شه اضافه کرد
+                // که آن ها را مقداردهی کرده ایم
+                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var message = JsonConvert.SerializeObject(publishModel);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.DeliveryMode = 2;
+
+                /// در اینجا بعد از ساخت صف و درست کردن پیام ارسالی 
+                /// زمان آن رسیده است که آن را انتشار بدیم، برای اینکار
+                /// یک سری پارامترها رو باید ست کنیم، در اینجا نوع ایکس چنج 
+                /// را حالت پیش فرض در نظر گرفته ایم
+                channel.ConfirmSelect();
+                channel.BasicPublish(exchange: "", routingKey: queueName, mandatory: true, basicProperties: properties, body: body);
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += (sender, eventArgs) =>
+                  {
+                      Console.WriteLine("Sent RabbitMQ");
+                    // implement ack handle
+                };
+                channel.ConfirmSelect();
+            }
+        }
+    }
+}
