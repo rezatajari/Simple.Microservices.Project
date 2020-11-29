fb5441713261d81f8918aa9664336ca4604d0157
rezatajari <reza.tajari70@gmail.com>
2020-11-29 19:09:13 +0330

    Create RabbitMQ sections
    - Create EventBusRabbitMQ
    - Create IRabbitMQConnection
    - Create RabbitMQConnection

---
+++ src/Common/EventBusRabbitMQ/IRabbitMQConnection.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ
+{
+    public interface IRabbitMQConnection:IDisposable
+    {
+        bool IsConnected { get; }
+        bool TryConnect();
+        IModel CreateModel();
+    }
+}
---
+++ src/Common/EventBusRabbitMQ/RabbitMQConnection.cs
@@ -0,0 +1,86 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EventBusRabbitMQ
+{
+    public class RabbitMQConnection : IRabbitMQConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private bool _disposed;
+
+        public RabbitMQConnection(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+            if (!IsConnected)
+                TryConnect();
+        }
+
+        /// <summary>
+        /// بررسی شرط برقراری ارتباط
+        /// </summary>
+        public bool IsConnected
+        {
+            get
+            {
+                return _connection != null && _connection.IsOpen && !_disposed;
+            }
+        }
+
+        /// <summary>
+        /// اگه ارتباط برقرار نباشد در این متد برقراری ارتباط انجام می گیرد
+        /// </summary>
+        /// <returns></returns>
+        public bool TryConnect()
+        {
+            try
+            {
+                _connection = _connectionFactory.CreateConnection();
+            }
+            catch (BrokerUnreachableException)
+            {
+                Thread.Sleep(2000);
+                _connection = _connectionFactory.CreateConnection();
+            }
+
+            if (IsConnected)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// مدل ارتباطی
+        /// </summary>
+        /// <returns></returns>
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+                throw new InvalidOperationException("No rabbit connection");
+
+            return _connection.CreateModel();
+        }
+
+        /// <summary>
+        /// پاکسازی منابع غیر قابل استفاده در حافظه
+        /// </summary>
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            try
+            {
+                _connection.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
