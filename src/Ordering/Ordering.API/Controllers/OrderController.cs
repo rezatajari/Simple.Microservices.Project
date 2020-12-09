a68648627ea137d02cce5c1e7cebc8ed265809bc
rezatajari <reza.tajari70@gmail.com>
2020-12-09 21:39:09 +0330

    Start Ordering.API section
    - start to  created prj
    - created OrderController empty

---
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ordering.API.Controllers
+{
+    [Route(template:"[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+
+
+
+
+
+    }
+}
