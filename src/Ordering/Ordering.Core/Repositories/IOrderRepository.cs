39b0e0543e8fae1868fa6da855b2a8b735fe5ba7
rezatajari <reza.tajari70@gmail.com>
2020-12-29 21:08:48 +0330

    Created IRepository and Repository

---
+++ src/Ordering/Ordering.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Base;
+
+namespace Ordering.Core.Repositories
+{
+    public interface IOrderRepository:IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrderByUserName(string userName);
+    }
+}
