c121f89d7419dd25f30e2edc7d1dcc5c3ab56e59
rezatajari <reza.tajari70@gmail.com>
2020-12-30 21:18:55 +0330

    Create OrderContext and more
    OrderContext
    OrderContextSeed
    set Program in Ordering.API

---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+  public  class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILoggerFactory loggerFactory,
+            int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                orderContext.Database.Migrate();
+
+                if (!orderContext.Orders.Any())
+                {
+                    orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                    await orderContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 3)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<OrderContextSeed>();
+                    log.LogError(exception.Message);
+                    await SeedAsync(orderContext, loggerFactory, retryForAvailability);
+                }
+
+                throw;
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>()
+            {
+                new Order() { UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "meh@ozk.com", AddressLine = "Bahcelievler", TotalPrice = 5239 },
+                new Order() { UserName = "swn", FirstName = "Selim", LastName = "Arslan", EmailAddress ="sel@ars.com", AddressLine = "Ferah", TotalPrice = 3486 }
+            };
+        }
+  }
+}
